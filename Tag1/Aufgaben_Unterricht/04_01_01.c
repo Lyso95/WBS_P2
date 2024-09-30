eaef5bf05cb9abc32ec8c917923d4ecfc2577c36
Lyso95 <amos@langensiepen.net>
2024-09-30 12:05:23 +0200

    Repository für P2-Kurs initialisiert -> Ordner Tag 1 erstellt  - Aufgabe 04_01_01 testweise erstellt um .gitignore zu testen

---
+++ Tag1/Aufgaben_Unterricht/04_01_01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+    int x;
+    srand(time(NULL));
+    rand();
+    
+    // Würfeln
+    x = rand()%6+1;
+
+    if(x>3)
+    {
+        printf("Überdurchschnittlich");
+    }
+    else
+    {
+        switch(x)
+        {
+            case 1: printf("Oh Mann!"); break;
+            case 2: printf("Naja"); break;
+            case 3: printf("O. K."); break;
+            default: printf("Häh??");
+        }
+    }
+    printf("\n");
+    return 0;
+}
